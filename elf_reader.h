9172ea0004254921c757589a6423fb77427b5505
Shen Sijie <sjshen@pku.edu.cn>
2018-04-06 21:07:57 +0800

    Load executable file part finished

---
+++ elf_reader.h
@@ -0,0 +1,18 @@
+//
+// Name: elf_reader
+// Project: RISC_V_Simulator
+// Author: Shen Sijie
+// Date: 4/6/18
+//
+
+#ifndef RISC_V_SIMULATOR_ELF_READER_H
+#define RISC_V_SIMULATOR_ELF_READER_H
+
+#include "utility.h"
+#include "machine.h"
+#include <elf.h>
+
+#define EM_RISCV 0xF3
+
+
+#endif //RISC_V_SIMULATOR_ELF_READER_H
